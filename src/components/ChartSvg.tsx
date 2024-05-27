b5c677a373fe60b19225e3e8c3e1ea6b639488cc
Abdulmalik Almasud <mlk1321.aa@gmail.com>
2024-05-28 00:34:49 +0300

    adding ChartSvg

---
+++ src/components/ChartSvg.tsx
@@ -0,0 +1,479 @@
+export function ChartSvg() {
+  return (
+    <>
+      <svg
+        className="recharts-surface"
+        width="685"
+        height="350"
+        viewBox="0 0 685 350">
+        <title></title>
+        <desc></desc>
+        <defs>
+          <clipPath id="recharts20-clip">
+            <rect x="65" y="5" height="310" width="615"></rect>
+          </clipPath>
+        </defs>
+        <g className="recharts-layer recharts-cartesian-axis recharts-xAxis xAxis">
+          <g className="recharts-cartesian-axis-ticks">
+            <g className="recharts-layer recharts-cartesian-axis-tick">
+              <text
+                stroke="none"
+                fontSize="12"
+                orientation="bottom"
+                width="615"
+                height="30"
+                x="90.625"
+                y="323"
+                fill="#888888"
+                className="recharts-text recharts-cartesian-axis-tick-value"
+                textAnchor="middle">
+                <tspan x="90.625" dy="0.71em">
+                  Jan
+                </tspan>
+              </text>
+            </g>
+            <g className="recharts-layer recharts-cartesian-axis-tick">
+              <text
+                stroke="none"
+                fontSize="12"
+                orientation="bottom"
+                width="615"
+                height="30"
+                x="141.875"
+                y="323"
+                fill="#888888"
+                className="recharts-text recharts-cartesian-axis-tick-value"
+                textAnchor="middle">
+                <tspan x="141.875" dy="0.71em">
+                  Feb
+                </tspan>
+              </text>
+            </g>
+            <g className="recharts-layer recharts-cartesian-axis-tick">
+              <text
+                stroke="none"
+                fontSize="12"
+                orientation="bottom"
+                width="615"
+                height="30"
+                x="193.125"
+                y="323"
+                fill="#888888"
+                className="recharts-text recharts-cartesian-axis-tick-value"
+                textAnchor="middle">
+                <tspan x="193.125" dy="0.71em">
+                  Mar
+                </tspan>
+              </text>
+            </g>
+            <g className="recharts-layer recharts-cartesian-axis-tick">
+              <text
+                stroke="none"
+                fontSize="12"
+                orientation="bottom"
+                width="615"
+                height="30"
+                x="244.375"
+                y="323"
+                fill="#888888"
+                className="recharts-text recharts-cartesian-axis-tick-value"
+                textAnchor="middle">
+                <tspan x="244.375" dy="0.71em">
+                  Apr
+                </tspan>
+              </text>
+            </g>
+            <g className="recharts-layer recharts-cartesian-axis-tick">
+              <text
+                stroke="none"
+                fontSize="12"
+                orientation="bottom"
+                width="615"
+                height="30"
+                x="295.625"
+                y="323"
+                fill="#888888"
+                className="recharts-text recharts-cartesian-axis-tick-value"
+                textAnchor="middle">
+                <tspan x="295.625" dy="0.71em">
+                  May
+                </tspan>
+              </text>
+            </g>
+            <g className="recharts-layer recharts-cartesian-axis-tick">
+              <text
+                stroke="none"
+                fontSize="12"
+                orientation="bottom"
+                width="615"
+                height="30"
+                x="346.875"
+                y="323"
+                fill="#888888"
+                className="recharts-text recharts-cartesian-axis-tick-value"
+                textAnchor="middle">
+                <tspan x="346.875" dy="0.71em">
+                  Jun
+                </tspan>
+              </text>
+            </g>
+            <g className="recharts-layer recharts-cartesian-axis-tick">
+              <text
+                stroke="none"
+                fontSize="12"
+                orientation="bottom"
+                width="615"
+                height="30"
+                x="398.125"
+                y="323"
+                fill="#888888"
+                className="recharts-text recharts-cartesian-axis-tick-value"
+                textAnchor="middle">
+                <tspan x="398.125" dy="0.71em">
+                  Jul
+                </tspan>
+              </text>
+            </g>
+            <g className="recharts-layer recharts-cartesian-axis-tick">
+              <text
+                stroke="none"
+                fontSize="12"
+                orientation="bottom"
+                width="615"
+                height="30"
+                x="449.375"
+                y="323"
+                fill="#888888"
+                className="recharts-text recharts-cartesian-axis-tick-value"
+                textAnchor="middle">
+                <tspan x="449.375" dy="0.71em">
+                  Aug
+                </tspan>
+              </text>
+            </g>
+            <g className="recharts-layer recharts-cartesian-axis-tick">
+              <text
+                stroke="none"
+                fontSize="12"
+                orientation="bottom"
+                width="615"
+                height="30"
+                x="500.625"
+                y="323"
+                fill="#888888"
+                className="recharts-text recharts-cartesian-axis-tick-value"
+                textAnchor="middle">
+                <tspan x="500.625" dy="0.71em">
+                  Sep
+                </tspan>
+              </text>
+            </g>
+            <g className="recharts-layer recharts-cartesian-axis-tick">
+              <text
+                stroke="none"
+                fontSize="12"
+                orientation="bottom"
+                width="615"
+                height="30"
+                x="551.875"
+                y="323"
+                fill="#888888"
+                className="recharts-text recharts-cartesian-axis-tick-value"
+                textAnchor="middle">
+                <tspan x="551.875" dy="0.71em">
+                  Oct
+                </tspan>
+              </text>
+            </g>
+            <g className="recharts-layer recharts-cartesian-axis-tick">
+              <text
+                stroke="none"
+                fontSize="12"
+                orientation="bottom"
+                width="615"
+                height="30"
+                x="603.125"
+                y="323"
+                fill="#888888"
+                className="recharts-text recharts-cartesian-axis-tick-value"
+                textAnchor="middle">
+                <tspan x="603.125" dy="0.71em">
+                  Nov
+                </tspan>
+              </text>
+            </g>
+            <g className="recharts-layer recharts-cartesian-axis-tick">
+              <text
+                stroke="none"
+                fontSize="12"
+                orientation="bottom"
+                width="615"
+                height="30"
+                x="654.375"
+                y="323"
+                fill="#888888"
+                className="recharts-text recharts-cartesian-axis-tick-value"
+                textAnchor="middle">
+                <tspan x="654.375" dy="0.71em">
+                  Dec
+                </tspan>
+              </text>
+            </g>
+          </g>
+        </g>
+        <g className="recharts-layer recharts-cartesian-axis recharts-yAxis yAxis">
+          <g className="recharts-cartesian-axis-ticks">
+            <g className="recharts-layer recharts-cartesian-axis-tick">
+              <text
+                stroke="none"
+                fontSize="12"
+                orientation="left"
+                width="60"
+                height="310"
+                x="57"
+                y="315"
+                fill="#888888"
+                className="recharts-text recharts-cartesian-axis-tick-value"
+                textAnchor="end">
+                <tspan x="57" dy="0.355em">
+                  $0
+                </tspan>
+              </text>
+            </g>
+            <g className="recharts-layer recharts-cartesian-axis-tick">
+              <text
+                stroke="none"
+                fontSize="12"
+                orientation="left"
+                width="60"
+                height="310"
+                x="57"
+                y="237.5"
+                fill="#888888"
+                className="recharts-text recharts-cartesian-axis-tick-value"
+                textAnchor="end">
+                <tspan x="57" dy="0.355em">
+                  $1500
+                </tspan>
+              </text>
+            </g>
+            <g className="recharts-layer recharts-cartesian-axis-tick">
+              <text
+                stroke="none"
+                fontSize="12"
+                orientation="left"
+                width="60"
+                height="310"
+                x="57"
+                y="160"
+                fill="#888888"
+                className="recharts-text recharts-cartesian-axis-tick-value"
+                textAnchor="end">
+                <tspan x="57" dy="0.355em">
+                  $3000
+                </tspan>
+              </text>
+            </g>
+            <g className="recharts-layer recharts-cartesian-axis-tick">
+              <text
+                stroke="none"
+                fontSize="12"
+                orientation="left"
+                width="60"
+                height="310"
+                x="57"
+                y="82.5"
+                fill="#888888"
+                className="recharts-text recharts-cartesian-axis-tick-value"
+                textAnchor="end">
+                <tspan x="57" dy="0.355em">
+                  $4500
+                </tspan>
+              </text>
+            </g>
+            <g className="recharts-layer recharts-cartesian-axis-tick">
+              <text
+                stroke="none"
+                fontSize="12"
+                orientation="left"
+                width="60"
+                height="310"
+                x="57"
+                y="9"
+                fill="#888888"
+                className="recharts-text recharts-cartesian-axis-tick-value"
+                textAnchor="end">
+                <tspan x="57" dy="0.355em">
+                  $6000
+                </tspan>
+              </text>
+            </g>
+          </g>
+        </g>
+        <g className="recharts-layer recharts-bar fill-primary">
+          <g className="recharts-layer recharts-bar-rectangles">
+            <g className="recharts-layer">
+              <g className="recharts-layer recharts-bar-rectangle" role="img">
+                <path
+                  fill="currentColor"
+                  radius="4,4,0,0"
+                  className="recharts-rectangle fill-primary"
+                  width="41"
+                  height="74.19333333333336"
+                  name="Jan"
+                  x="70.125"
+                  y="240.80666666666664"
+                  d="M70.125,244.80666666666664A 4,4,0,0,1,74.125,240.80666666666664L 107.125,240.80666666666664A 4,4,0,0,1,
+        111.125,244.80666666666664L 111.125,315L 70.125,315Z"></path>
+              </g>
+              <g className="recharts-layer recharts-bar-rectangle" role="img">
+                <path
+                  fill="currentColor"
+                  radius="4,4,0,0"
+                  className="recharts-rectangle fill-primary"
+                  width="41"
+                  height="79.51499999999999"
+                  name="Feb"
+                  x="121.375"
+                  y="235.485"
+                  d="M121.375,239.485A 4,4,0,0,1,125.375,235.485L 158.375,235.485A 4,4,0,0,1,
+        162.375,239.485L 162.375,315L 121.375,315Z"></path>
+              </g>
+              <g className="recharts-layer recharts-bar-rectangle" role="img">
+                <path
+                  fill="currentColor"
+                  radius="4,4,0,0"
+                  className="recharts-rectangle fill-primary"
+                  width="41"
+                  height="224.38833333333332"
+                  name="Mar"
+                  x="172.625"
+                  y="90.61166666666668"
+                  d="M172.625,94.61166666666668A 4,4,0,0,1,176.625,90.61166666666668L 209.625,90.61166666666668A 4,4,0,0,1,
+        213.625,94.61166666666668L 213.625,315L 172.625,315Z"></path>
+              </g>
+              <g className="recharts-layer recharts-bar-rectangle" role="img">
+                <path
+                  fill="currentColor"
+                  radius="4,4,0,0"
+                  className="recharts-rectangle fill-primary"
+                  width="41"
+                  height="113.15"
+                  name="Apr"
+                  x="223.875"
+                  y="201.85"
+                  d="M223.875,205.85A 4,4,0,0,1,227.875,201.85L 260.875,201.85A 4,4,0,0,1,
+        264.875,205.85L 264.875,315L 223.875,315Z"></path>
+              </g>
+              <g className="recharts-layer recharts-bar-rectangle" role="img">
+                <path
+                  fill="currentColor"
+                  radius="4,4,0,0"
+                  className="recharts-rectangle fill-primary"
+                  width="41"
+                  height="114.70000000000002"
+                  name="May"
+                  x="275.125"
+                  y="200.29999999999998"
+                  d="M275.125,204.29999999999998A 4,4,0,0,1,279.125,200.29999999999998L 312.125,200.29999999999998A 4,4,0,0,1,
+        316.125,204.29999999999998L 316.125,315L 275.125,315Z"></path>
+              </g>
+              <g className="recharts-layer recharts-bar-rectangle" role="img">
+                <path
+                  fill="currentColor"
+                  radius="4,4,0,0"
+                  className="recharts-rectangle fill-primary"
+                  width="41"
+                  height="54.56"
+                  name="Jun"
+                  x="326.375"
+                  y="260.44"
+                  d="M326.375,264.44A 4,4,0,0,1,330.375,260.44L 363.375,260.44A 4,4,0,0,1,
+        367.375,264.44L 367.375,315L 326.375,315Z"></path>
+              </g>
+              <g className="recharts-layer recharts-bar-rectangle" role="img">
+                <path
+                  fill="currentColor"
+                  radius="4,4,0,0"
+                  className="recharts-rectangle fill-primary"
+                  width="41"
+                  height="125.34333333333333"
+                  name="Jul"
+                  x="377.625"
+                  y="189.65666666666667"
+                  d="M377.625,193.65666666666667A 4,4,0,0,1,381.625,189.65666666666667L 414.625,189.65666666666667A 4,4,0,0,1,
+        418.625,193.65666666666667L 418.625,315L 377.625,315Z"></path>
+              </g>
+              <g className="recharts-layer recharts-bar-rectangle" role="img">
+                <path
+                  fill="currentColor"
+                  radius="4,4,0,0"
+                  className="recharts-rectangle fill-primary"
+                  width="41"
+                  height="218.65333333333334"
+                  name="Aug"
+                  x="428.875"
+                  y="96.34666666666666"
+                  d="M428.875,100.34666666666666A 4,4,0,0,1,432.875,96.34666666666666L 465.875,96.34666666666666A 4,4,0,0,1,
+        469.875,100.34666666666666L 469.875,315L 428.875,315Z"></path>
+              </g>
+              <g className="recharts-layer recharts-bar-rectangle" role="img">
+                <path
+                  fill="currentColor"
+                  radius="4,4,0,0"
+                  className="recharts-rectangle fill-primary"
+                  width="41"
+                  height="247.94833333333332"
+                  name="Sep"
+                  x="480.125"
+                  y="67.05166666666668"
+                  d="M480.125,71.05166666666668A 4,4,0,0,1,484.125,67.05166666666668L 517.125,67.05166666666668A 4,4,0,0,1,
+        521.125,71.05166666666668L 521.125,315L 480.125,315Z"></path>
+              </g>
+              <g className="recharts-layer recharts-bar-rectangle" role="img">
+                <path
+                  fill="currentColor"
+                  radius="4,4,0,0"
+                  className="recharts-rectangle fill-primary"
+                  width="41"
+                  height="276.62333333333333"
+                  name="Oct"
+                  x="531.375"
+                  y="38.376666666666665"
+                  d="M531.375,42.376666666666665A 4,4,0,0,1,535.375,38.376666666666665L 568.375,38.376666666666665A 4,4,0,0,1,
+        572.375,42.376666666666665L 572.375,315L 531.375,315Z"></path>
+              </g>
+              <g className="recharts-layer recharts-bar-rectangle" role="img">
+                <path
+                  fill="currentColor"
+                  radius="4,4,0,0"
+                  className="recharts-rectangle fill-primary"
+                  width="41"
+                  height="160.0633333333333"
+                  name="Nov"
+                  x="582.625"
+                  y="154.9366666666667"
+                  d="M582.625,158.9366666666667A 4,4,0,0,1,586.625,154.9366666666667L 619.625,154.9366666666667A 4,4,0,0,1,
+        623.625,158.9366666666667L 623.625,315L 582.625,315Z"></path>
+              </g>
+              <g className="recharts-layer recharts-bar-rectangle" role="img">
+                <path
+                  fill="currentColor"
+                  radius="4,4,0,0"
+                  className="recharts-rectangle fill-primary"
+                  width="41"
+                  height="105.6583333333333"
+                  name="Dec"
+                  x="633.875"
+                  y="209.3416666666667"
+                  d="M633.875,213.3416666666667A 4,4,0,0,1,637.875,209.3416666666667L 670.875,209.3416666666667A 4,4,0,0,1,
+        674.875,213.3416666666667L 674.875,315L 633.875,315Z"></path>
+              </g>
+            </g>
+          </g>
+          <g className="recharts-layer"></g>
+        </g>
+      </svg>
+    </>
+  );
+}
